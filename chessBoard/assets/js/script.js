a5017a3b6f95b8107248aec3567f2f11998f2f66
Timothy Hoang <Timothy Hoang>
2017-04-14 17:43:27 -0500

    first commit

---
+++ chessBoard/assets/js/script.js
@@ -0,0 +1,14 @@
+// Write a program that creates a string that represents an 8x8 grid, using newline characters to separate lines. 
+// At each position of the grid there is either a space or a "#" character. THe characters should form a chess board.
+
+// # # # #
+//  # # # #
+// # # # #
+//  # # # #
+// # # # #
+//  # # # #
+// # # # #
+
+// When you have a program that generates this pattern, define a variable size = 8 and 
+// change the program so that it works for any size, the program so that it works for any size,
+// outputting a grid of the given width and height.
---
+++ loopingATriangle/assets/js/script.js
@@ -0,0 +1,14 @@
+// // Write a loop that makes seven calls to console.log to output the triangles
+// #
+// ##
+// ###
+// ####
+// #####
+// ######
+// #######
+
+var s = "";
+for (i=1; i<8; i++) {
+	s = s + "#";
+	console.log(s);
+}
